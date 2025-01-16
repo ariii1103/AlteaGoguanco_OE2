3658d4103e61ebdc9155ec167c8f9f72523c55a2
Gabriel Dominic K. Altea <altea.gabriel@yahoo.com>
2025-01-16 17:21:23 +0800

    css and js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Select elements
+const productForm = document.getElementById('productForm');
+const productName = document.getElementById('productName');
+const productDescription = document.getElementById('productDescription');
+const productPrice = document.getElementById('productPrice');
+const productImage = document.getElementById('productImage');
+const productPreview = document.getElementById('productPreview');
+const message = document.getElementById('message');
+const productList = document.getElementById('productList');
+
+// Store uploaded products
+let products = [];
+
+//
